2a848342ca7c37e9974245ce6980b6683cca1bec
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2014-09-16 19:15:33 +0200

    Use a singe post subscription to ensure that we can always see the right post.

--- universe/lib/router.js
+++ universe/lib/router.js
@@ -41,7 +41,10 @@ Router.map(function() {
   this.route('postPage', {
     path: '/posts/:_id',
     waitOn: function() {
-      return Meteor.subscribe('comments', this.params._id);
+      return [
+        Meteor.subscribe('singlePost', this.params._id),
+        Meteor.subscribe('comments', this.params._id)
+      ];
     },
     data: function() { return Posts.findOne(this.params._id);   }
   });
@@ -51,6 +54,9 @@ Router.map(function() {
   });
   this.route('postEdit', {
     path: '/posts/:_id/edit',
+    waitOn: function() { 
+      return Meteor.subscribe('singlePost', this.params._id);
+    },
     data: function() { return Posts.findOne(this.params._id); }
   });
 });
--- universe/server/publications.js
+++ universe/server/publications.js
@@ -3,6 +3,10 @@ Meteor.publish('posts', function(sort, limit){
   return Posts.find({}, {sort: sort, limit: limit});
 });
 
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
+
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
 });
