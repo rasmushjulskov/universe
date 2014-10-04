929905d40031eb46323e057368b3dc153e3168af
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2014-10-05 00:19:41 +0200

    Added sticker collection

--- universe/client/views/upload/upload.js
+++ universe/client/views/upload/upload.js
@@ -13,61 +13,101 @@ Template.upload.helpers({
 
 Template.upload.events({
   'click #uploadSave': function(){
-    var pos = {
-      X: newPosX,
-      Y: newPosY        
-    }
-    Meteor.call('update', pos, function(error, id) {
+    $(".box").each(function(){
+
+      var pos = {
+        id: $(this).attr("data-id"),
+        X: $(this).attr("data-top"),
+        Y: $(this).attr("data-left")    
+      }
+    
+      Meteor.call('update', pos, function(error, id) {
+        if(error) {
+          return alert(error.reason);
+        } else {
+        }
+        //Router.go('postPage', {_id: id});
+      }); 
+    });
+  },
+  'click #reset': function(){
+    $(".box").each(function(){
+      var pos = {
+        id: $(this).attr("data-id"),
+        X: 0,
+        Y: 0    
+      }
+      Meteor.call('update', pos, function(error, id) {
+        if(error) {
+          return alert(error.reason);
+        } else {
+        }
+        //Router.go('postPage', {_id: id});
+      }); 
+    });
+  },
+  'click #addElement': function(){
+    Meteor.call('add', function(error, id) {
       if(error) {
         return alert(error.reason);
-      } else {
       }
-      //Router.go('postPage', {_id: id});
     }); 
   }
 });
 
+
 Template.upload.rendered = function(){
+
   // Animates to the current position
-  var i =$("#box1");
-  TweenLite.to(i, 0.4, {left:i.attr("data-left"), top: i.attr("data-top")});
+  $(".box").each(function(){
+    TweenLite.to($(this), 0.4, {left: $(this).attr("data-left"), top: $(this).attr("data-top")});
+  });
+  
 
   var parent = $("#container"),
     startX, startY;
 
-  Draggable.create("#box1, #box2", {
+  Draggable.create(".box", {
     bounds: parent,
-    edgeResistance:0.65,
+    edgeResistance:0.8,
     throwProps:true,
     onPress:function() {
-      //record the starting values so we can compare them later...
-
       
       startX = this.x,
       startY = this.y        
-      console.log(this)
     },
     onDrag:function() {
+      
       newPosX = this.x;
       newPosY = this.y;
+      $(this).attr("data-left", this.x);
+      $(this).attr("data-top", this.y);
     },
     onThrowComplete: function() {
       TweenLite.to("#original", 0.7, {x:this.x, y:this.y, ease:Power2.easeInOut});
     }
   });
-/*
-  function getMomentaryDirection(target) {
-    var x = ThrowPropsPlugin.getVelocity(target, "x"),
-        y = ThrowPropsPlugin.getVelocity(target, "y"),
-        ratio = Math.abs(x / y),
-        direction = [];
-    if (ratio > 0.25) { //you can adjust the ratio thresholds to make things more or less sensitive to diagonal movement.
-      direction.push((x < 0) ? "left" : "right");
-    }
-    if (ratio < 4) {
-      direction.push((y < 0) ? "up" : "down");
-    }
-    return direction.join("-");
-  }
-*/
+
+/*  if(Meteor.isClient){
+    Deps.autorun(function(){
+      var i = Stickers.findOne({postId: "wZAc6vk7FWSiXeqN9"});
+    
+      var left = i.left;
+      var top = i.top;
+      alert("wokr")
+      TweenLite.to(i, 0.4, {left: left, top: top});
+    });
+  }*/
+  $("input[name=scale]").change(function(){
+    TweenLite.to($("#container"), 1,{scale: $(this).val()});
+  });
+  $("input[name=rotate]").change(function(){
+    TweenLite.to($("#container"), 1,{rotation: $(this).val()});
+  });
+  $("input[name=skewX]").change(function(){
+    TweenLite.to($("#container"), 1,{skewX: $(this).val()});
+  });
+  $("input[name=skewY]").change(function(){
+    TweenLite.to($("#container"), 1,{skewY: $(this).val()});
+  });
 };
--- universe/collections/stickers.js
+++ universe/collections/stickers.js
@@ -1,8 +1,11 @@
 Stickers = new Meteor.Collection('stickers');
 
 Meteor.methods({
-  update: function(attr) {
-    Stickers.update({"_id" : "wZAc6vk7FWSiXeqN9"}, {$set: {"top" : attr.X, "left" : attr.Y}});
+  update: function(ie) {
+    Stickers.update({"_id" : ie.id}, {$set: {"top" : ie.X, "left" : ie.Y}});
+  },
+  add: function(id) {
+    Stickers.insert({ "title" : "element2", "top" : 50, "left" : 50});
   }
 });
 
